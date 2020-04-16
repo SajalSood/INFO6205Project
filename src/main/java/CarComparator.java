be53f1b6850a49002f42ade48c94b0a8e64b0af3
RitujaM <rituja.v.mahajan@gmail.com>
2020-04-16 19:55:45 -0400

    {-}
    #Priority Queue code fixed
    #Code Refactored.

--- src/main/java/CarComparator.java
+++ src/main/java/CarComparator.java
@@ -1,18 +1,12 @@
 import java.util.Comparator;
 
 public class CarComparator implements Comparator<Car> {
-
     @Override
     public int compare(Car car1, Car car2) {
-       if(car1.getLaneNumber()=="Third" && car2.getLaneNumber()=="Second")
-           return 1;
-        if(car1.getLaneNumber()=="Second" && car2.getLaneNumber()=="Third" )
+        if (car1.getPriority() < car2.getPriority())
+            return 1;
+        else if (car1.getPriority() > car2.getPriority())
             return -1;
-        if(car2.getLaneNumber()=="Second" && car1.getLaneNumber()=="Second" )
-            return 0;
-        if(car2.getLaneNumber()=="Third" && car1.getLaneNumber()=="Third" )
-            return 0;
-
         return 0;
     }
 }
